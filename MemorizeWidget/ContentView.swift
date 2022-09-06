17c42c600b07133eb4786ce5bbc8e1f7fb428d89
aaaaa <aaaaa>
2022-09-06 11:53:24 +0900

    Initial Commit

---
+++ MemorizeWidget/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MemorizeWidget
+//
+//  Created by Mei Sei on 2022/09/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MemorizeWidget/MemorizeWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeWidgetApp.swift
+//  MemorizeWidget
+//
+//  Created by Mei Sei on 2022/09/06.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
