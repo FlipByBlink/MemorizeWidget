14b02c359553da5bffd3751c97ba0ffdc9af3096
aaaaa <aaaaa>
2022-09-08 15:31:25 +0900

    a

--- MemorizeWidget/ContentView.swift
+++ MemorizeWidget/ContentView.swift
@@ -112,10 +112,18 @@ struct 📓NoteRow: View {
 
 struct 🛠MenuTab: View {
     @EnvironmentObject var 📱: 📱AppModel
+    @State private var 🚩ImportFile: Bool = false
     
     var body: some View {
         NavigationView {
             List {
+                Toggle(isOn: 📱.$🚩ShowComment) {
+                    Label("Show comment on widget", systemImage: "list.dash.header.rectangle")
+                }
+                .onChange(of: 📱.🚩ShowComment) { _ in
+                    WidgetCenter.shared.reloadAllTimelines()
+                }
+                
                 Toggle(isOn: 📱.$🚩RectangularBackground) {
                     Label("Show rectangular background on lock screen", systemImage: "rectangle.dashed")
                 }
@@ -123,11 +131,18 @@ struct 🛠MenuTab: View {
                     WidgetCenter.shared.reloadAllTimelines()
                 }
                 
-                Toggle(isOn: 📱.$🚩ShowComment) {
-                    Label("Show comment on widget", systemImage: "list.dash.header.rectangle")
+                Button {
+                    🚩ImportFile = true
+                } label: {
+                    Label("Import TSV file", systemImage: "arrow.down.doc")
                 }
-                .onChange(of: 📱.🚩ShowComment) { _ in
-                    WidgetCenter.shared.reloadAllTimelines()
+                .fileImporter(isPresented: $🚩ImportFile, allowedContentTypes: [.tabSeparatedText]) { ⓡesult in
+                    switch ⓡesult { //TODO: 実装
+                        case .success(let success):
+                            print(success)
+                        case .failure(let failure):
+                            print(failure)
+                    }
                 }
             }
             .navigationTitle("Menu")
