9893e36e7dfc328c88817baa5e3ccfe5fc2eb4da
aaaaa <aaaaa>
2022-10-30 11:07:51 +0900

    if #available(iOS 16.0, *) {             NavigationStack {

--- MemorizeWidget/ContentView.swift
+++ MemorizeWidget/ContentView.swift
@@ -398,57 +398,62 @@ struct 🛒PurchaseTab: View {
 
 struct ℹ️AboutAppTab: View {
     var body: some View {
-        NavigationView {
-            List {
-                Section {
-                    ZStack {
-                        Color.clear
-                        VStack(spacing: 12) {
-                            Image("ClipedIcon")
-                                .resizable()
-                                .shadow(radius: 4, y: 1)
-                                .frame(width: 100, height: 100)
-                            VStack(spacing: 6) {
-                                Text("MemorizeWidget")
-                                    .font(.system(.title2, design: .rounded))
-                                    .fontWeight(.bold)
-                                    .tracking(1.5)
-                                    .opacity(0.75)
-                                    .lineLimit(1)
-                                    .minimumScaleFactor(0.1)
-                                Text("Application for iPhone / iPad")
-                                    .font(.footnote)
-                                    .fontWeight(.medium)
-                                    .foregroundStyle(.secondary)
-                            }
-                            .lineLimit(1)
-                            .minimumScaleFactor(0.1)
-                        }
-                        .padding(48)
-                    }
-                }
-                
-                Section {
-                    Link(destination: 🔗AppStoreProductURL) {
-                        HStack {
-                            Label("Open AppStore page", systemImage: "link")
-                            Spacer()
-                            Image(systemName: "arrow.up.forward.app")
-                                .imageScale(.small)
+        if #available(iOS 16.0, *) {
+            NavigationStack {
+                🄻ist()
+                    .toolbar(.visible, for: .navigationBar)
+            }
+        } else {
+            NavigationView { 🄻ist() }
+                .navigationViewStyle(StackNavigationViewStyle())
+        }
+    }
+    func 🄻ist() -> some View {
+        List {
+            Section {
+                ZStack {
+                    Color.clear
+                    VStack(spacing: 12) {
+                        Image("ClipedIcon")
+                            .resizable()
+                            .shadow(radius: 4, y: 1)
+                            .frame(width: 100, height: 100)
+                        VStack(spacing: 6) {
+                            Text("MemorizeWidget")
+                                .font(.system(.title2, design: .rounded))
+                                .fontWeight(.bold)
+                                .tracking(1.5)
+                                .opacity(0.75)
+                                .lineLimit(1)
+                                .minimumScaleFactor(0.1)
+                            Text("Application for iPhone / iPad")
+                                .font(.footnote)
+                                .fontWeight(.medium)
                                 .foregroundStyle(.secondary)
                         }
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.1)
                     }
-                    
-                    NavigationLink {
-                        ℹ️AboutAppMenu()
-                    } label: {
-                        Label("About App", systemImage: "doc")
-                    }
+                    .padding(48)
                 }
             }
-            .navigationBarTitleDisplayMode(.inline)
+            Section {
+                Link(destination: 🔗AppStoreProductURL) {
+                    HStack {
+                        Label("Open AppStore page", systemImage: "link")
+                        Spacer()
+                        Image(systemName: "arrow.up.forward.app")
+                            .imageScale(.small)
+                            .foregroundStyle(.secondary)
+                    }
+                }
+                NavigationLink {
+                    ℹ️AboutAppMenu()
+                } label: {
+                    Label("About App", systemImage: "doc")
+                }
+            }
         }
-        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
