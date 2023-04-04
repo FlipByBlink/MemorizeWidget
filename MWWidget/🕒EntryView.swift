a711ab62319069ea40b3c79ddc06ec76ea1bb229
aaaaa <aaaaa>
2023-04-04 12:57:47 +0900

    private struct 🄷omeScreenWidgetView: View {

--- MWWidget/🕒EntryView.swift
+++ MWWidget/🕒EntryView.swift
@@ -11,7 +11,7 @@ struct 🅆idgetEntryView: View {
             Group {
                 switch self.widgetFamily {
                     case .systemSmall, .systemMedium, .systemLarge:
-                        self.ⓗomeScreenWidgetView()
+                        🄷omeScreenWidgetView(self.ⓘnfo)
                     case .accessoryCorner, .accessoryInline:
                         self.ⓐccessoryOneLineView()
                     case .accessoryCircular:
@@ -25,67 +25,6 @@ struct 🅆idgetEntryView: View {
             .widgetURL(self.ⓘnfo.url)
         } else {
             self.ⓝoNoteView()
-        }
-    }
-    private func ⓗomeScreenWidgetView() -> some View {
-        Group {
-            var ⓣitleFont: Font {
-                switch self.widgetFamily {
-                    case .systemSmall, .systemMedium:
-                        return self.ⓝotes.count == 1 ? .title3 : .headline
-                    case .systemLarge:
-                        return self.ⓝotes.count == 1 ? .largeTitle : .title2
-                    default:
-                        return .largeTitle
-                }
-            }
-            var ⓒommentFont: Font {
-                switch self.widgetFamily {
-                    case .systemSmall, .systemMedium:
-                        return self.ⓝotes.count == 1 ? .subheadline : .caption
-                    case .systemLarge:
-                        return self.ⓝotes.count == 1 ? .title2 : .body
-                    default:
-                        return .body
-                }
-            }
-            var ⓛineLimit: Int {
-                switch self.widgetFamily {
-                    case .systemSmall, .systemMedium:
-                        return self.ⓝotes.count == 1 ? 3 : 1
-                    case .systemLarge:
-                        return self.ⓝotes.count < 4 ? 3 : 1
-                    default:
-                        return 1
-                }
-            }
-            ZStack {
-                Color.clear
-                VStack(spacing: 8) {
-                    Spacer(minLength: 0)
-                    ForEach(self.ⓝotes) { ⓝote in
-                        VStack(spacing: self.ⓝotes.count == 1 ? 8 : 2) {
-                            Text(ⓝote.title)
-                                .font(ⓣitleFont.bold())
-                            if self.🚩showComment {
-                                if !ⓝote.comment.isEmpty {
-                                    Text(ⓝote.comment)
-                                        .font(ⓒommentFont.weight(.light))
-                                        .foregroundStyle(.secondary)
-                                } else {
-                                    Color.clear
-                                        .frame(height: 6)
-                                }
-                            }
-                        }
-                        .lineLimit(ⓛineLimit)
-                        .minimumScaleFactor(0.5)
-                        .multilineTextAlignment(.center)
-                    }
-                    Spacer(minLength: 0)
-                }
-                .padding()
-            }
         }
     }
     private func ⓐccessoryOneLineView() -> some View {
@@ -147,5 +86,74 @@ struct 🅆idgetEntryView: View {
     }
     init(_ ⓔntry: 🕒WidgetEntry) {
         self.ⓘnfo = ⓔntry.info
+    }
+}
+
+private struct 🄷omeScreenWidgetView: View {
+    private var ⓘnfo: 🪧WidgetInfo
+    @Environment(\.widgetFamily) var widgetFamily
+    @AppStorage("ShowComment", store: .ⓐppGroup) var 🚩showComment: Bool = false
+    private var ⓝotes: [📗Note] { self.ⓘnfo.notes }
+    private var ⓣitleFont: Font {
+        switch self.widgetFamily {
+            case .systemSmall, .systemMedium:
+                return self.ⓝotes.count == 1 ? .title3 : .headline
+            case .systemLarge:
+                return self.ⓝotes.count == 1 ? .largeTitle : .title2
+            default:
+                return .largeTitle
+        }
+    }
+    private var ⓒommentFont: Font {
+        switch self.widgetFamily {
+            case .systemSmall, .systemMedium:
+                return self.ⓝotes.count == 1 ? .subheadline : .caption
+            case .systemLarge:
+                return self.ⓝotes.count == 1 ? .title2 : .body
+            default:
+                return .body
+        }
+    }
+    private var ⓛineLimit: Int {
+        switch self.widgetFamily {
+            case .systemSmall, .systemMedium:
+                return self.ⓝotes.count == 1 ? 3 : 1
+            case .systemLarge:
+                return self.ⓝotes.count < 4 ? 3 : 1
+            default:
+                return 1
+        }
+    }
+    var body: some View {
+        ZStack {
+            Color.clear
+            VStack(spacing: 8) {
+                Spacer(minLength: 0)
+                ForEach(self.ⓝotes) { ⓝote in
+                    VStack(spacing: self.ⓝotes.count == 1 ? 8 : 2) {
+                        Text(ⓝote.title)
+                            .font(self.ⓣitleFont.bold())
+                        if self.🚩showComment {
+                            if !ⓝote.comment.isEmpty {
+                                Text(ⓝote.comment)
+                                    .font(self.ⓒommentFont.weight(.light))
+                                    .foregroundStyle(.secondary)
+                            } else {
+                                Color.clear
+                                    .frame(height: 6)
+                            }
+                        }
+                    }
+                    .lineLimit(self.ⓛineLimit)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+                }
+                Spacer(minLength: 0)
+            }
+            .padding()
+        }
+    }
+    init(_ info: 🪧WidgetInfo) {
+        self.ⓘnfo = info
     }
 }
