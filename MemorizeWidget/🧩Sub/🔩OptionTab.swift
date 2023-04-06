f8a97bcf2d1d0e2dfb72fa8c93edcc8c531c6639
aaaaa <aaaaa>
2023-04-06 11:01:49 +0900

    a

--- MemorizeWidget/🧩Sub/🔩OptionTab.swift
+++ MemorizeWidget/🧩Sub/🔩OptionTab.swift
@@ -9,7 +9,7 @@ struct 🔩OptionTab: View {
                 💬CommentOnWidgetSection()
                 🔍CustomizeSearchSection()
                 🚮DeleteAllNotesButton()
-                🗑TrashMenuLink()
+                🗑TrashSection()
                 📤ExportNotesSection()
                 if #available(iOS 16.0, *) { 🄳irectionsSection() }
             }
@@ -93,35 +93,60 @@ private struct 🏞BeforeAfterImage: View {
 }
 
 private struct 🔍CustomizeSearchSection: View {
-    @AppStorage("SearchLeadingText") var 🔗leading: String = ""
-    @AppStorage("SearchTrailingText") var 🔗trailing: String = ""
-    private var ⓔntireText: String {
-        let ⓛeading = self.🔗leading.isEmpty ? "https://duckduckgo.com/?q=" : self.🔗leading
-        return ⓛeading + "NOTETITLE" + self.🔗trailing
-    }
     var body: some View {
         Section {
-            VStack {
-                Text(self.ⓔntireText)
-                    .italic()
-                    .font(.system(.footnote, design: .monospaced))
-                    .multilineTextAlignment(.center)
-                    .padding(8)
-                    .frame(minHeight: 100)
-                    .animation(.default, value: self.🔗leading.isEmpty)
-                    .foregroundStyle(self.🔗leading.isEmpty ? .secondary : .primary)
-                TextField("URL scheme", text: self.$🔗leading)
-                TextField("Trailing component", text: self.$🔗trailing)
-                    .font(.caption)
-                    .padding(.bottom, 4)
+            NavigationLink {
+                Self.🄼enu()
+            } label: {
+                Label("Customize search button", systemImage: "magnifyingglass")
             }
-            .textFieldStyle(.roundedBorder)
-        } header: {
-            Label("Customize search", systemImage: "magnifyingglass")
-        } footer: {
-            Text("Pre-installed shortcut to search in DuckDuckGo.")
         }
-        .headerProminence(.increased)
+    }
+    private struct 🄼enu: View {
+        @EnvironmentObject var 📱: 📱AppModel
+        @AppStorage("SearchLeadingText") var 🔗leading: String = ""
+        @AppStorage("SearchTrailingText") var 🔗trailing: String = ""
+        private var ⓔntireText: String {
+            let ⓛeading = self.🔗leading.isEmpty ? "https://duckduckgo.com/?q=" : self.🔗leading
+            return ⓛeading + "NOTETITLE" + self.🔗trailing
+        }
+        var body: some View {
+            List {
+                Section {
+                    VStack {
+                        Text(self.ⓔntireText)
+                            .italic()
+                            .font(.system(.caption, design: .monospaced))
+                            .multilineTextAlignment(.center)
+                            .padding(8)
+                            .frame(minHeight: 100)
+                            .animation(.default, value: self.🔗leading.isEmpty)
+                            .foregroundStyle(self.🔗leading.isEmpty ? .secondary : .primary)
+                        TextField("Leading component", text: self.$🔗leading)
+                        TextField("Trailing component", text: self.$🔗trailing)
+                            .font(.caption)
+                            .padding(.bottom, 4)
+                    }
+                    .textFieldStyle(.roundedBorder)
+                } header: {
+                    Text("Customize URL scheme")
+                }
+                Section {
+                    🔍SearchButton(📗Note("NOTETITLE"))
+                } header: {
+                    Text("Test")
+                }
+                Section {
+                    Text("Pre-installed shortcut to search in DuckDuckGo.")
+                        .font(.subheadline)
+                        .foregroundStyle(.secondary)
+                        .padding(.vertical, 4)
+                } header: {
+                    Text("Pre-install")
+                }
+            }
+            .navigationTitle("Search button")
+        }
     }
 }
 
@@ -147,119 +172,118 @@ private struct 🚮DeleteAllNotesButton: View {
     }
 }
 
-private struct 🗑TrashMenuLink: View {
+private struct 🗑TrashSection: View {
     @EnvironmentObject var 📱: 📱AppModel
     var body: some View {
         NavigationLink {
-            🗑TrashMenu()
+            Self.🄼enu()
         } label: {
             Label("Trash", systemImage: "trash")
                 .badge(📱.🗑trash.deletedContents.count)
         }
     }
-}
-
-private struct 🗑TrashMenu: View {
-    @EnvironmentObject var 📱: 📱AppModel
-    var body: some View {
-        List {
-            ForEach(📱.🗑trash.deletedContents) {
-                self.ⓒontentSection($0)
+    private struct 🄼enu: View {
+        @EnvironmentObject var 📱: 📱AppModel
+        var body: some View {
+            List {
+                ForEach(📱.🗑trash.deletedContents) {
+                    self.ⓒontentSection($0)
+                }
+                self.ⓔmptyTrashView()
+                self.ⓐboutTrashSection()
             }
-            self.ⓔmptyTrashView()
-            self.ⓐboutTrashSection()
+            .navigationTitle("Trash")
+            .toolbar { self.ⓒlearButton() }
+            .animation(.default, value: 📱.🗑trash.deletedContents)
         }
-        .navigationTitle("Trash")
-        .toolbar { self.ⓒlearButton() }
-        .animation(.default, value: 📱.🗑trash.deletedContents)
-    }
-    private func ⓒontentSection(_ ⓒontent: 🄳eletedContent) -> some View {
-        Section {
-            if ⓒontent.notes.count == 1 {
-                self.ⓢingleNoteRow(ⓒontent)
-            } else {
-                self.ⓜultiNotesRows(ⓒontent)
+        private func ⓒontentSection(_ ⓒontent: 🄳eletedContent) -> some View {
+            Section {
+                if ⓒontent.notes.count == 1 {
+                    self.ⓢingleNoteRow(ⓒontent)
+                } else {
+                    self.ⓜultiNotesRows(ⓒontent)
+                }
+            } header: {
+                Text(ⓒontent.date, style: .offset)
+                +
+                Text(" (\(ⓒontent.date.formatted(.dateTime.month().day().hour().minute())))")
             }
-        } header: {
-            Text(ⓒontent.date, style: .offset)
-            +
-            Text(" (\(ⓒontent.date.formatted(.dateTime.month().day().hour().minute())))")
         }
-    }
-    private func ⓢingleNoteRow(_ ⓒontent: 🄳eletedContent) -> some View {
-        HStack {
-            self.ⓝoteView(ⓒontent.notes.first ?? .init("🐛"))
-            Spacer()
-            self.ⓡestoreButton(ⓒontent)
-                .labelStyle(.iconOnly)
-                .buttonStyle(.plain)
-                .font(.title)
-                .symbolRenderingMode(.hierarchical)
-                .foregroundColor(.secondary)
-                .padding(4)
-        }
-    }
-    private func ⓜultiNotesRows(_ ⓒontent: 🄳eletedContent) -> some View {
-        Group {
-            ForEach(ⓒontent.notes) { self.ⓝoteView($0) }
-            self.ⓡestoreButton(ⓒontent)
-        }
-    }
-    private func ⓝoteView(_ ⓝote: 📗Note) -> some View {
-        HStack {
-            VStack(alignment: .leading, spacing: 2) {
-                Text(ⓝote.title)
-                    .font(.headline)
-                Text(ⓝote.comment)
-                    .font(.subheadline)
-                    .foregroundStyle(.secondary)
+        private func ⓢingleNoteRow(_ ⓒontent: 🄳eletedContent) -> some View {
+            HStack {
+                self.ⓝoteView(ⓒontent.notes.first ?? .init("🐛"))
+                Spacer()
+                self.ⓡestoreButton(ⓒontent)
+                    .labelStyle(.iconOnly)
+                    .buttonStyle(.plain)
+                    .font(.title)
+                    .symbolRenderingMode(.hierarchical)
+                    .foregroundColor(.secondary)
+                    .padding(4)
             }
-            .padding(8)
         }
-    }
-    private func ⓡestoreButton(_ ⓒontent: 🄳eletedContent) -> some View {
-        Button {
-            📱.restore(ⓒontent)
-        } label: {
-            Label("Restore", systemImage: "arrow.uturn.backward.circle.fill")
+        private func ⓜultiNotesRows(_ ⓒontent: 🄳eletedContent) -> some View {
+            Group {
+                ForEach(ⓒontent.notes) { self.ⓝoteView($0) }
+                self.ⓡestoreButton(ⓒontent)
+            }
         }
-    }
-    private func ⓒlearButton() -> some View {
-        Menu {
-            Button(role: .destructive) {
-                📱.🗑trash.clearDeletedContents()
+        private func ⓝoteView(_ ⓝote: 📗Note) -> some View {
+            HStack {
+                VStack(alignment: .leading, spacing: 2) {
+                    Text(ⓝote.title)
+                        .font(.headline)
+                    Text(ⓝote.comment)
+                        .font(.subheadline)
+                        .foregroundStyle(.secondary)
+                }
+                .padding(8)
+            }
+        }
+        private func ⓡestoreButton(_ ⓒontent: 🄳eletedContent) -> some View {
+            Button {
+                📱.restore(ⓒontent)
+            } label: {
+                Label("Restore", systemImage: "arrow.uturn.backward.circle.fill")
+            }
+        }
+        private func ⓒlearButton() -> some View {
+            Menu {
+                Button(role: .destructive) {
+                    📱.🗑trash.clearDeletedContents()
+                } label: {
+                    Label("Clear trash", systemImage: "trash.slash")
+                }
             } label: {
                 Label("Clear trash", systemImage: "trash.slash")
             }
-        } label: {
-            Label("Clear trash", systemImage: "trash.slash")
+            .tint(.red)
+            .disabled(📱.🗑trash.deletedContents.isEmpty)
         }
-        .tint(.red)
-        .disabled(📱.🗑trash.deletedContents.isEmpty)
-    }
-    private func ⓔmptyTrashView() -> some View {
-        Group {
-            if 📱.🗑trash.deletedContents.isEmpty {
-                ZStack {
-                    Color.clear
-                    Label("Empty", systemImage: "xmark.bin")
-                        .font(.title2)
-                        .foregroundStyle(.secondary)
+        private func ⓔmptyTrashView() -> some View {
+            Group {
+                if 📱.🗑trash.deletedContents.isEmpty {
+                    ZStack {
+                        Color.clear
+                        Label("Empty", systemImage: "xmark.bin")
+                            .font(.title2)
+                            .foregroundStyle(.secondary)
+                    }
+                    .padding(32)
+                    .listRowBackground(Color.clear)
                 }
-                .padding(32)
-                .listRowBackground(Color.clear)
             }
         }
-    }
-    private func ⓐboutTrashSection() -> some View {
-        Section {
-            Label("After 7 days, the notes will be permanently deleted.",
-                  systemImage: "clock.badge.exclamationmark")
-            Label("Trash do not sync with iCloud.", systemImage: "xmark.icloud")
+        private func ⓐboutTrashSection() -> some View {
+            Section {
+                Label("After 7 days, the notes will be permanently deleted.",
+                      systemImage: "clock.badge.exclamationmark")
+                Label("Trash do not sync with iCloud.", systemImage: "xmark.icloud")
+            }
+            .font(.subheadline)
+            .foregroundStyle(.secondary)
+            .listRowBackground(Color.clear)
         }
-        .font(.subheadline)
-        .foregroundStyle(.secondary)
-        .listRowBackground(Color.clear)
     }
 }
 
@@ -288,7 +312,7 @@ private struct 📤ExportNotesSection: View {
             List {
                 Section {
                     Text(self.ⓣext)
-                        .font(.subheadline.monospaced())
+                        .font(.subheadline.monospaced().italic())
                         .textSelection(.enabled)
                         .padding()
                     if #available(iOS 16.0, *) {
