649fb56e46fe45e5ba0197cd1c6c3ca426400248
aaaa <a@a>
2023-11-16 11:50:03 +0900

    a

--- Shared/🪧Widget/Rest/🪧SubWidget.swift
+++ Shared/🪧Widget/Rest/🪧SubWidget.swift
@@ -9,6 +9,7 @@ struct 🪧SubWidget: Widget {
                 🪧PlaceholderView()
             } else {
                 🪧EntryView($0)
+                    .modifier(Self.SnapshotTitle(condition: $0.phase == .snapshot))
             }
         }
         .configurationDisplayName("Sub widget")
@@ -30,5 +31,47 @@ private extension 🪧SubWidget {
         ⓥalue.append(.accessoryCorner)
 #endif
         return ⓥalue
+    }
+    private struct SnapshotTitle: ViewModifier {
+        @Environment(\.widgetFamily) var widgetFamily
+        var condition: Bool
+        func body(content: Content) -> some View {
+            if self.condition {
+                content
+                    .overlay {
+                        ZStack(alignment: .bottom) {
+                            Color.clear
+                            switch self.widgetFamily {
+#if os(iOS) || os(macOS)
+                                case .systemSmall, .systemMedium, .systemLarge, .systemExtraLarge:
+                                    Text("Sub")
+                                        .font(.caption)
+                                        .fontWeight(.medium)
+                                        .padding(4)
+#if os(iOS)
+                                        .foregroundStyle(.secondary)
+#endif
+                                        .background(.regularMaterial)
+                                        .clipShape(RoundedRectangle(cornerRadius: 6))
+                                        .padding(8)
+#endif
+#if os(iOS) || os(watchOS)
+                                case .accessoryCircular, .accessoryRectangular:
+                                    Text("Sub")
+                                        .underline()
+                                        .foregroundStyle(.secondary)
+                                        .font(.caption2.weight(.semibold))
+                                        .lineLimit(1)
+                                        .minimumScaleFactor(0.6)
+#endif
+                                default:
+                                    EmptyView()
+                            }
+                        }
+                    }
+            } else {
+                content
+            }
+        }
     }
 }
