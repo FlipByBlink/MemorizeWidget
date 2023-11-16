0f08b9c399fa86a63e80179587a3c940b41a3a81
aaaa <a@a>
2023-11-16 09:06:56 +0900

    WIP

---
+++ Shared/🎛️Option/🎛️Default.swift
@@ -0,0 +1,12 @@
+enum 🎛️Default { //MARK: Work in progress
+    enum FontSize {
+        enum SystemFamily {
+            static let title = 24
+            static let comment = 14
+        }
+        enum AccessoryFamily {
+            static let title = 18
+            static let comment = 10
+        }
+    }
+}
--- Shared/🎛️Option/🎛️DefaultValue.swift
+++
@@ -1,8 +0,0 @@
-//
-//  🎛️DefaultValue.swift
-//  MemorizeWidget
-//
-//  Created by FULLNAME on 2023/11/16.
-//
-
-import Foundation
