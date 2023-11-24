0e02541027cc9df1c0fab1d2821855bd6b4f3c21
aaaa <a@a>
2023-11-24 19:29:20 +0900

    Workaround(macOS 14.1.1): There is a bug that the last line of multiple lines is hidden. This problem was resolved by specifying the font size with a specific numerical value.

--- macOS/📚NotesWindow/📗NoteRow.swift
+++ macOS/📚NotesWindow/📗NoteRow.swift
@@ -4,21 +4,23 @@ struct 📗NoteRow: View {
     @EnvironmentObject var model: 📱AppModel
     @Binding var source: 📗Note
     var body: some View {
-        HStack(spacing: 4) {
-            VStack(spacing: 2) {
+        HStack(spacing: 8) {
+            VStack(spacing: 4) {
                 TextField("No title", text: self.$source.title, axis: .vertical)
-                    .font(.title3.bold())
+                    .font(.system(size: 19))
+                    .bold()
                 TextField("No comment", text: self.$source.comment, axis: .vertical)
                     .disabled(self.source.title.isEmpty)
                     .font(.body.weight(.light))
+                    .foregroundStyle(.secondary)
             }
             .focusedValue(\.editingNote, self.source)
             .onSubmit { self.model.submitTextField(self.source) }
             Self.RemoveButton(self.source)
         }
-        .padding(.vertical, 6)
-        .padding(.leading, 6)
-        .padding(.trailing, 2)
+        .padding(.vertical, 12)
+        .padding(.leading, 12)
+        .padding(.trailing, 4)
     }
 }
 
@@ -46,3 +48,5 @@ private extension 📗NoteRow {
         }
     }
 }
+
+//Workaround(macOS 14.1.1): There is a bug that the last line of multiple lines is hidden. This problem was resolved by specifying the font size with a specific numerical value.
