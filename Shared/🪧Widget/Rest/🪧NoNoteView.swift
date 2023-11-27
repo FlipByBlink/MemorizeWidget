00424efdb7107a8b89ef768872114a62f63031f6
aaaa <a@a>
2023-11-27 16:36:54 +0900

    a

--- Shared/🪧Widget/Rest/🪧NoNoteView.swift
+++ Shared/🪧Widget/Rest/🪧NoNoteView.swift
@@ -4,31 +4,34 @@ struct 🪧NoNoteView: View {
     @Environment(\.widgetFamily) var widgetFamily
     @Environment(\.showsWidgetContainerBackground) var showsWidgetContainerBackground
     var body: some View {
-        Group {
-            switch self.widgetFamily {
-                case .systemSmall, .systemMedium:
-                    Label("No note", systemImage: "book.closed")
-                        .font(.title)
-                        .foregroundStyle(
-                            self.showsWidgetContainerBackground ? .tertiary : .secondary
-                        )
-                case .systemLarge, .systemExtraLarge:
-                    Label("No note", systemImage: "book.closed")
-                        .font(.largeTitle)
-                        .foregroundStyle(.tertiary)
-                case .accessoryRectangular:
-                    Label("No note", systemImage: "book.closed")
-                        .font(.headline)
-                        .foregroundStyle(.secondary)
-                case .accessoryCircular:
-                    Text("No note")
-                        .font(.subheadline.weight(.semibold))
-                        .foregroundStyle(.secondary)
-                case .accessoryInline, .accessoryCorner:
-                    Label("No note", systemImage: "book.closed")
-                default:
-                    Image(systemName: "book.closed")
-            }
+        switch self.widgetFamily {
+            case .systemSmall:
+                Label("No note", systemImage: "book.closed")
+                    .font(.title2)
+                    .foregroundStyle(self.showsWidgetContainerBackground ? .tertiary : .secondary)
+                    .padding(8)
+            case .systemMedium:
+                Label("No note", systemImage: "book.closed")
+                    .font(.title)
+                    .foregroundStyle(.tertiary)
+                    .padding()
+            case .systemLarge, .systemExtraLarge:
+                Label("No note", systemImage: "book.closed")
+                    .font(.largeTitle)
+                    .foregroundStyle(.tertiary)
+                    .padding()
+            case .accessoryRectangular:
+                Label("No note", systemImage: "book.closed")
+                    .font(.headline)
+                    .foregroundStyle(.secondary)
+            case .accessoryCircular:
+                Text("No note")
+                    .font(.subheadline.weight(.semibold))
+                    .foregroundStyle(.secondary)
+            case .accessoryInline, .accessoryCorner:
+                Label("No note", systemImage: "book.closed")
+            default:
+                Image(systemName: "book.closed")
         }
     }
 }
